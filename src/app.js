b7b1c8fd53cbf7116e5d996b67c268d614a90d04
SwaroopSRP <srp31.swaroop@gmail.com>
2025-08-16 19:28:17 +0530

    Add app.js and setup Express.js configs

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+export default app;
+
+// Basic Middleware Configs
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// CORS Configs
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    }),
+);
+
+app.get("/", (req, res) => {
+    res.status(200).send(
+        "Welcome! You are now accessing an instance of Zenithic's core.",
+    );
+});
--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({
     path: "./.env",
 });
 
-console.log(process.env.author);
-console.log("Hello World!");
+const port = process.env.PORT || 6969;
+
+try {
+    app.listen(port, () => {
+        console.log(
+            `Zenithic Core server up and listening on http://localhost:${port}`,
+        );
+    });
+} catch (error) {
+    console.warn("Zenithic Core server failed to initialize!");
+    console.error(error);
+}
