63ec8da2c7193a55dba7f5e621637d91e8d56c9b
SwaroopSRP <srp31.swaroop@gmail.com>
2025-08-18 16:33:54 +0530

    Add utils/async_handler.js and update healthchecker.controller.js according to it

--- src/controllers/healthchecker.controller.js
+++ src/controllers/healthchecker.controller.js
@@ -1,16 +1,13 @@
 import ServerResponse from "../utils/api_response.js";
+import asyncHandler from "../utils/async_handler.js";
 
-function healthChecker(req, res) {
-    try {
-        res.status(200).json(
-            new ServerResponse(200, {
-                info: "Server is running...",
-                status: "UP"
-            })
-        );
-    } catch (err) {
-        console.error(err);
-    }
-}
+const healthChecker = asyncHandler(async (req, res) => {
+    res.status(200).send(
+        new ServerResponse(200, {
+            message: "Server up and running...",
+            status: "UP"
+        })
+    );
+});
 
 export default healthChecker;
