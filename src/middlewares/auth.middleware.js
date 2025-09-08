c9445da89f43652ef6bbfd3ebd5e78b90cf857fe
SwaroopSRP <srp31.swaroop@gmail.com>
2025-09-08 23:25:27 +0530

    Add auth.middleware.js: Implement function for checking of access token

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+import userModel from "../models/user.model.js";
+import ServerError from "../utils/api_error.js";
+import asyncHandler from "../utils/async_handler.js";
+import jwt from "jsonwebtoken";
+
+const verifyAccessToken = asyncHandler(async (req, res, next) => {
+    const token = req.cookies?.accessToken;
+    if (!token) {
+        throw new ServerError(
+            401,
+            "Unauthorized request! Access token is missing."
+        );
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await userModel
+            .findById(decoded?._id)
+            .select("-passwd -refreshToken -emailVerifToken -emailVerifExpiry");
+        if (!user) {
+            throw new ServerError(401, "Invalid access token!");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error);
+        throw new ServerError(401, "Invalid access token!");
+    }
+});
+
+export default verifyAccessToken;
